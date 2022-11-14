433a30558d055ca30299de5e9266a9a3ad1f3bd2
tunayev <mtuna61@gmail.com>
2022-11-14 21:51:56 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Test your speed!</h1>
+  <button @click="start" class="mt-5 bg-green-300" :disabled="started">
+    Start!
+  </button>
+  <Block v-if="started" :delay="delay" />
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+export default {
+  components: { Block },
+  name: "App",
+  methods: {
+    start() {
+      this.started = true;
+      this.delay = 1000 + Math.random() * 4000;
+    },
+  },
+  data() {
+    return {
+      started: false,
+      delay: null,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="h-[200px] w-[400px] bg-blue-200 rounded-xl text-center my-10"
+    v-if="showBlock"
+  ></div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+    }, this.delay);
+  },
+};
+</script>
+
+<style></style>
