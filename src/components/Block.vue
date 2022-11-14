fac6b0f50b14ea7e16cc403829f3fc44cf187d44
tunayev <mtuna61@gmail.com>
2022-11-14 22:05:40 +0300

    timers added

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -2,6 +2,7 @@
   <div
     class="h-[200px] w-[400px] bg-blue-200 rounded-xl text-center my-10"
     v-if="showBlock"
+    @click="stopTimer"
   ></div>
 </template>
 
@@ -11,12 +12,26 @@ export default {
   data() {
     return {
       showBlock: false,
+      timer: null,
+      reactionTime: 0,
     };
   },
   mounted() {
     setTimeout(() => {
       this.showBlock = true;
+      this.startTimer();
     }, this.delay);
+  },
+
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+    },
   },
 };
 </script>
